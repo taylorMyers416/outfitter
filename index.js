913a418744621dee26cabf8d523685f1e8b30431
taylorMyers416 <taylorMyers416@gmail.com>
2019-02-27 19:31:16 -0500

    deploying to github pages

--- index.js
+++ index.js
@@ -7,12 +7,12 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var functions = require('firebase-functions');
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 3000;
-// Sets up the Express app to handle data parsing
-
+var cors = require('cors')({origin: true});
+app.use(cors);
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({
     extended: true
@@ -24,20 +24,19 @@ app.use(express.static("public"));
 // Routes
 // =============================================================
 app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/html/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html/"));
 });
 app.get("/closet", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/html/closet.html"));
+    res.sendFile(path.join(__dirname, "../public/html/closet.html/"));
 });
 app.get("/add-clothing", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/html/add-clothing.html"));
+    res.sendFile(path.join(__dirname, "../public/html/add-clothing.html/"));
 });
 app.get("/create-outfit", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/html/create-outfit.html"));
+    res.sendFile(path.join(__dirname, "../public/html/create-outfit.html"));
 });
 app.get("/generate-outfits", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/html/generate-outfits.html"));
+    res.sendFile(path.join(__dirname, "../public/html/generate-outfits.html"));
 });
-app.listen(PORT, function () {
-    console.log("App listening on PORT https://localhost:" + PORT);
-});
+
+exports.app = functions.https.onRequest(app);
